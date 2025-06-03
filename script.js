c3f77437af0a26f2c74551eddd0520c38e791313
Duduoop <dudu.van.veen@gmail.com>
2025-06-03 23:44:22 +0200

    adding files, ps not finished

---
+++ script.js
@@ -0,0 +1,29 @@
+const containerEl = codument.querySelector(".container");
+
+for (let index=0; index < 30; index++) {
+    const colorContainerEL = document.createElement("div");
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl);
+}
+
+const colorContainerEls = document.querySelectorAll(".color-container");
+
+generatorCOlors();
+
+function generateColors() {
+    colorContainersEls.forEach((colorContainerEl)=> {
+        const newColorCode = randomColor();
+        colorContainerEL.style.backgroundColor = "#" + newColorCode;
+        colorContainerEl.innerText = "#" + newColorCode;
+    });
+}
+
+function randomColor () {
+    const chars = "0123456789abcdef";
+    const colorCodeLength = 6;
+    let colorCode = "";
+    for (let index = 0; index < colorCodeLenght; index++) {
+        const randomNum = Math.floor(Math.random() * chars.length);
+        colorCode += chars.substring(randomNum, randomNum + 1);
+    }
+}
